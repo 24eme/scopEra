1a426d1fa7f3fd24ff54da036ac433181ac80465
MNGOM <mngom@24eme.fr>
2020-07-17 13:49:05 +0200

    Mis en commun

--- js/data.js
+++ js/data.js
@@ -1,5 +1,6 @@
 // https://www.legifrance.gouv.fr/affichTexte.do?cidTexte=JORFTEXT000038369660&dateTexte=&categorieLien=id
 // https://www.coordonnees-gps.fr/
+alert('la');
 var marker = L.icon({
     iconUrl: 'css/img/marker.png',
     iconSize: [35, 30],
@@ -11,6 +12,194 @@ function onEachFeature(feature, layer) {
         layer.setIcon(marker).addTo(map);
     }
 };
+alert('la');
+$("#health").click(function() {
+  alert('ici');
+});
+
+var scops = $.getJSON("cooperative.json");
+scops.then(function(data) {
+      var allscops = L.geoJson(data);
+
+      var culture = L.geoJson(data, {
+            filter: function(feature, layer) {
+                return feature.properties.field == "Culture";
+            },
+            pointToLayer: function(feature, latlng) {
+                return L.marker(latlng, {
+                    icon: cafeIcon
+                }).on('mouseover', function() {
+                    this.bindPopup(feature.properties.name).openPopup();
+                });
+            }
+      });
+      var commerce = L.geoJson(data, {
+            filter: function(feature, layer) {
+                return feature.properties.field == "Commerce";
+            },
+            pointToLayer: function(feature, latlng) {
+                return L.marker(latlng, {
+                    icon: cafeIcon
+                }).on('mouseover', function() {
+                    this.bindPopup(feature.properties.name).openPopup();
+                });
+            }
+      });
+      var education = L.geoJson(data, {
+            filter: function(feature, layer) {
+                return feature.properties.field == "Education";
+            },
+            pointToLayer: function(feature, latlng) {
+                return L.marker(latlng, {
+                    icon: cafeIcon
+                }).on('mouseover', function() {
+                    this.bindPopup(feature.properties.name).openPopup();
+                });
+            }
+      });
+      var social = L.geoJson(data, {
+            filter: function(feature, layer) {
+                return feature.properties.field == "Social";
+            },
+            pointToLayer: function(feature, latlng) {
+                return L.marker(latlng, {
+                    icon: cafeIcon
+                }).on('mouseover', function() {
+                    this.bindPopup(feature.properties.name).openPopup();
+                });
+            }
+      });
+      var health = L.geoJson(data, {
+            filter: function(feature, layer) {
+                return feature.properties.field == "Santé";
+            },
+            pointToLayer: function(feature, latlng) {
+                return L.marker(latlng, {
+                    icon: cafeIcon
+                }).on('mouseover', function() {
+                    this.bindPopup(feature.properties.name).openPopup();
+                });
+            }
+      });
+      var environment = L.geoJson(data, {
+            filter: function(feature, layer) {
+                return feature.properties.field == "Environnement";
+            },
+            pointToLayer: function(feature, latlng) {
+                return L.marker(latlng, {
+                    icon: cafeIcon
+                }).on('mouseover', function() {
+                    this.bindPopup(feature.properties.name).openPopup();
+                });
+            }
+      });
+      var others = L.geoJson(data, {
+            filter: function(feature, layer) {
+                return feature.properties.field != "Culture" "Commerce" "Education" "Social" "Sante" "Environnement" ;
+            },
+            pointToLayer: function(feature, latlng) {
+                return L.marker(latlng, {
+                }).on('mouseover', function() {
+                    this.bindPopup(feature.properties.name).openPopup();
+                });
+            }
+      });
+        map.fitBounds(allscops.getBounds(), {
+            padding: [50, 50]
+        });
+
+        culture.addTo(map);
+        commerce.addTo(map);
+        education.addTo(map);
+        social.addTo(map);
+        sante.addTo(map);
+        environment.addTo(map);
+        others.addTo(map);
+
+        $("#allscops").click(function() {
+            map.addLayer(allscops)
+            map.removeLayer(culture)
+            map.removeLayer(commerce)
+            map.removeLayer(education)
+            map.removeLayer(social)
+            map.removeLayer(sante)
+            map.removeLayer(environment)
+            map.removeLayer(others)
+        });
+        $("#culture").click(function() {
+            map.addLayer(culture)
+            map.removeLayer(allscops)
+            map.removeLayer(commerce)
+            map.removeLayer(education)
+            map.removeLayer(social)
+            map.removeLayer(sante)
+            map.removeLayer(environment)
+            map.removeLayer(others)
+        });
+        $("#commerce").click(function() {
+            map.addLayer(commerce)
+            map.removeLayer(allscops)
+            map.removeLayer(culture)
+            map.removeLayer(education)
+            map.removeLayer(social)
+            map.removeLayer(sante)
+            map.removeLayer(environment)
+            map.removeLayer(others)
+        });
+        $("#education").click(function() {
+            map.addLayer(education)
+            map.removeLayer(allscops)
+            map.removeLayer(culture)
+            map.removeLayer(commerce)
+            map.removeLayer(social)
+            map.removeLayer(sante)
+            map.removeLayer(environment)
+            map.removeLayer(others)
+        });
+        $("#social").click(function() {
+            map.addLayer(social)
+            map.removeLayer(allscops)
+            map.removeLayer(commerce)
+            map.removeLayer(education)
+            map.removeLayer(culture)
+            map.removeLayer(sante)
+            map.removeLayer(environment)
+            map.removeLayer(others)
+        });
+        $("#health").click(function() {
+          alert('ici');
+            map.addLayer(sante)
+            map.removeLayer(allscops)
+            map.removeLayer(commerce)
+            map.removeLayer(education)
+            map.removeLayer(social)
+            map.removeLayer(culture)
+            map.removeLayer(environment)
+            map.removeLayer(others)
+        });
+        $("#environment").click(function() {
+            map.addLayer(environment)
+            map.removeLayer(allscops)
+            map.removeLayer(commerce)
+            map.removeLayer(education)
+            map.removeLayer(social)
+            map.removeLayer(sante)
+            map.removeLayer(culture)
+            map.removeLayer(others)
+        });
+        $("#others").click(function() {
+          map.addLayer(others)
+          map.removeLayer(allscops)
+          map.removeLayer(commerce)
+          map.removeLayer(education)
+          map.removeLayer(social)
+          map.removeLayer(sante)
+          map.removeLayer(environment)
+          map.removeLayer(culture)
+        });
+    });
+
+var allscops = L.geoJSON()
 
 var scops = [
   {
