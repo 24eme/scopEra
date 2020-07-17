f88307c464183cb153dbb85826dd8aba97926771
MNGOM <mngom@24eme.fr>
2020-07-17 09:40:51 +0200

    -> GEOJSON et popup

--- js/data.js
+++ js/data.js
@@ -1,24 +1,128 @@
-//Remplir le jeu de données de scops
-
 var scops = [
-  [48.894436, 2.353057,'La Louve'],
-  [48.8492165, 2.3391038,'Coopérative laitière du Beaufortain'],
-  [48.894436, 2.353057,'Coopérative de la Goutte d\'Or'],
-  [48.8254034, 2.3414492,'24ème'],
-  [48.842192, 2.3255263,'ACOME'],
-  [48., 2.,'INITIS'],
-  [48., 2.,'IXREF'],
-  [48., 2.,'KOMOREBI'],
-  [48., 2.,'La Louve'],
-
+  {
+    "type": "Feature",
+    "properties": {
+        "name": "Coors Field",
+        "amenity": "Baseball Stadium",
+        "popupContent": "This is where the Rockies play!"
+    },
+    "geometry": {
+        "type": "Point",
+        "coordinates": [2.353057, 48.894436]
+    }
+  },
+  {
+    "type": "Feature",
+    "properties": {
+        "name": "Coors Field",
+        "amenity": "Baseball Stadium",
+        "popupContent": "This is where the Rockies play!"
+    },
+    "geometry": {
+        "type": "Point",
+        "coordinates": [2.3414492, 48.8254034]
+    }
+  },
+  {
+    "type": "Feature",
+    "properties": {
+        "name": "Coors Field",
+        "amenity": "Baseball Stadium",
+        "popupContent": "This is where the Rockies play!"
+    },
+    "geometry": {
+        "type": "Point",
+        "coordinates": [2.3414492, 48.8254034]
+    }
+  },
+  {
+    "type": "Feature",
+    "properties": {
+        "name": "Coors Field",
+        "amenity": "Baseball Stadium",
+        "popupContent": "This is where the Rockies play!"
+    },
+    "geometry": {
+        "type": "Point",
+        "coordinates": [2.3414492, 48.8254034]
+    }
+  },
+  {
+    "type": "Feature",
+    "properties": {
+        "name": "Coors Field",
+        "amenity": "Baseball Stadium",
+        "popupContent": "This is where the Rockies play!"
+    },
+    "geometry": {
+        "type": "Point",
+        "coordinates": [2.3414492, 48.8254034]
+    }
+  },
+  {
+    "type": "Feature",
+    "properties": {
+        "name": "Coors Field",
+        "amenity": "Baseball Stadium",
+        "popupContent": "This is where the Rockies play!"
+    },
+    "geometry": {
+        "type": "Point",
+        "coordinates": [2.3414492, 48.8254034]
+    }
+  },
+  {
+    "type": "Feature",
+    "properties": {
+        "name": "Coors Field",
+        "amenity": "Baseball Stadium",
+        "popupContent": "This is where the Rockies play!"
+    },
+    "geometry": {
+        "type": "Point",
+        "coordinates": [2.3414492, 48.8254034]
+    }
+  },
 ];
-//Fonction pour remplir la map
+
+// var greenIcon = L.icon({
+//     iconUrl: 'leaf-green.png',
+//     shadowUrl: 'leaf-shadow.png',
+//
+//     iconSize:     [38, 95], // size of the icon
+//     shadowSize:   [50, 64], // size of the shadow
+//     iconAnchor:   [22, 94], // point of the icon which will correspond to marker's location
+//     shadowAnchor: [4, 62],  // the same for the shadow
+//     popupAnchor:  [-3, -76] // point from which the popup should open relative to the iconAnchor
+// });
+// //
+// L.marker([51.5, -0.09], {icon: greenIcon}).addTo(map);
+//
+// var myStyle = {
+//     "color": "#ff7800",
+//     "weight": 5,
+//     "opacity": 0.65,
+// };
+//
+// L.geoJSON(scops, {
+//     style: myStyle
+// }).addTo(map);
+function onEachFeature(feature, layer) {
+    // does this feature have a property named popupContent?
+    if (feature.properties && feature.properties.popupContent) {
+        layer.bindPopup(feature.properties.popupContent);
+    }
+}
+L.geoJSON(scops, {
+    onEachFeature: onEachFeature
+}).addTo(map);
+
 function fillMap(data){
   for (var i = 0; i < data.length; i++) {
     var marker = L.marker(data[i]).addTo(map);
   }
 }
-
+//L.geoJSON(geojsonFeature).addTo(map);
 fillMap(scops);
 
 
