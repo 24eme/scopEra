b112943d44103ac11b809fdab7c2f422ee3df09e
Honorine Marie Christy <honorine@24eme.fr>
2020-07-17 11:19:22 +0200

    ajout de scops dans la variable scops

--- js/data.js
+++ js/data.js
@@ -2,7 +2,7 @@
 // https://www.coordonnees-gps.fr/
 var marker = L.icon({
     iconUrl: 'css/img/marker.png',
-    iconSize: [35, 30],
+    iconSize: [30, 30],
 });
 
 function onEachFeature(feature, layer) {
@@ -16,86 +16,99 @@ var scops = [
   {
     "type": "Feature",
     "properties": {
-        "name": "Coors Field",
-        "arrondisement": "Baseball Stadium",
-        "popupContent": "This is where the Rockies play!"
+        "name": "Yes High Tech",
+        "arrondisement": "0",
+        "secteur": "production",
+        "adresse":'20 Rue Saint-Joseph, 42000 Saint-Étienne, France',
+        "popupContent": "Yes High Tech"
     },
     "geometry": {
         "type": "Point",
-        "coordinates": [2.353057, 48.894436]
+        "coordinates": [4.375949859619141,45.4421501159668]
     }
   },
   {
     "type": "Feature",
     "properties": {
-        "name": "Coors Field",
-        "arrondisement": "Baseball Stadium",
-        "popupContent": "This is where the Rockies play!"
+        "name": "24ème",
+        "arrondisement": "9",
+        "secteur": "informatique",
+        'adresse':'40 rue Laffitte,75009 Paris',
+        "popupContent": "24ème"
     },
     "geometry": {
         "type": "Point",
-        "coordinates": [2.3414492, 48.8254034]
+        "coordinates": [2.3381113, 48.8740412]
     }
   },
   {
     "type": "Feature",
     "properties": {
-        "name": "Coors Field",
-        "arrondisement": "Baseball Stadium",
-        "popupContent": "This is where the Rockies play!"
+        "name": "DALIBO",
+        "arrondisement": "9",
+        "secteur": "informatique",
+        'adresse':'43 Rue du Faubourg Montmartre, 75009 Paris',
+        "popupContent": "DALIBO"
     },
     "geometry": {
         "type": "Point",
-        "coordinates": [2.3414492, 48.8254034]
+        "coordinates": [2.3415619, 48.8745253]
     }
   },
   {
     "type": "Feature",
     "properties": {
-        "name": "Coors Field",
-        "amenity": "Baseball Stadium",
-        "popupContent": "This is where the Rockies play!"
+        "name": "Bearstech",
+        "arrondisement": "0",
+        "secteur": "informatique",
+        'adresse':'40 Passage des Panoramas, Paris, France',
+        "popupContent": "Bearstech"
     },
     "geometry": {
         "type": "Point",
-        "coordinates": [2.3414492, 48.8254034]
+        "coordinates": [2.341618776321411, 48.87087631225586]
     }
   },
   {
     "type": "Feature",
     "properties": {
-        "name": "Coors Field",
-        "arrondisement": "Baseball Stadium",
-        "secteur": "",
-        "popupContent": "This is where the Rockies play!"
+        "name": "V-Technologies",
+        "arrondisement": "8",
+        "secteur": "informatique",
+        'adresse':'6 Avenue Franklin Delano Roosevelt, 75008 Paris',
+        "popupContent": "V-Technologies"
     },
     "geometry": {
         "type": "Point",
-        "coordinates": [2.3414492, 48.8254034]
+        "coordinates": [2.3100507259368896, 48.86988830566406]
     }
   },
   {
     "type": "Feature",
     "properties": {
-        "name": "Coors Field",
-        "amenity": "Baseball Stadium",
-        "popupContent": "This is where the Rockies play!"
+        "name": "SCOP IT",
+        "arrondisement": "11",
+        "secteur": "informatique",
+        'adresse':'5 Rue Richard Lenoir, 75011 Paris',
+        "popupContent": "SCOP IT"
     },
     "geometry": {
         "type": "Point",
-        "coordinates": [2.3414492, 48.8254034]
+        "coordinates": [2.3823288, 48.8543903]
     }
   },
   {
     "type": "Feature",
     "properties": {
-        "name": "Coors Field",
-        "amenity": "Baseball Stadium",
-        "popupContent": "This is where the Rockies play!"
+        "name": "Develop'it",
+        "arrondisement": "4",
+        "secteur": "informatique",
+        'adresse':'46 Rue Saint-Antoine, 75004 Paris',
+        "popupContent": "Develop'it"
     },
     "geometry": {
         "type": "Point",
-        "coordinates": [2.3414492, 48.8254034]
+        "coordinates": [2.3643888,48.8541803]
     }
   },
 ];
